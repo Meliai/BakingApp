46a155d63abdc5270989d5ffb41de9fd14796918
Helen Y <y23helen@gmail.com>
2017-09-15 10:48:25 +0300

    test expanded

--- app/src/androidTest/java/com/rudainc/bakingapp/MainTest.java
+++ app/src/androidTest/java/com/rudainc/bakingapp/MainTest.java
@@ -3,19 +3,26 @@ package com.rudainc.bakingapp;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
 import com.rudainc.bakingapp.activities.MainActivity;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 
 @RunWith(AndroidJUnit4.class)
 public class MainTest {
@@ -26,20 +33,27 @@ public class MainTest {
     @Test
     public void mainTest() {
 
+
+        onView(withIndex(withId(R.id.tv_recipe_name), 1)).check(matches(withText(containsString("Brownies"))));
+
         ViewInteraction recipes = onView(
                 allOf(withId(R.id.rv), isDisplayed()));
         recipes.perform(click());
 
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        onView(withId(R.id.title_ingredients)).check(matches(withText("Ingredients")));
 
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.fab),withContentDescription("Add widget"),
                         isDisplayed()));
         appCompatButton.perform(click());
+
+        onView(withIndex(withId(R.id.tv_step), 0)).check(matches(withText(containsString("Recipe Introduction"))));
 
         try {
             Thread.sleep(1000);
@@ -49,5 +63,24 @@ public class MainTest {
 
         onView(allOf(withId(R.id.step_description),isDisplayed()));
         onView(allOf(withId(R.id.playerView),isDisplayed()));
+    }
+
+
+    public static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
+        return new TypeSafeMatcher<View>() {
+            int currentIndex = 0;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with index: ");
+                description.appendValue(index);
+                matcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                return matcher.matches(view) && currentIndex++ == index;
+            }
+        };
     }
 }
